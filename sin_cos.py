6147b415423f52994a0174c2bf7917a3b3579afb
nshriva1 <47375019+nshriva1@users.noreply.github.com>
2019-02-05 22:28:24 -0500

    Add files via upload

---
+++ sin_cos.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = np.arange(0,10,0.1)
+sin = np.sin(time)
+cos = np.cos(time)
+plt.plot(time, sin, time, cos)
+plt.axhline(y=0, color = 'black')
+plt.show()
