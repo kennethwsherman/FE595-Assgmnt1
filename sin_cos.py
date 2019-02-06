0bafb157426191a5760d98ea4e2502714895b67c
nshriva1 <47375019+nshriva1@users.noreply.github.com>
2019-02-06 10:17:09 -0500

    Update sin_cos.py

--- sin_cos.py
+++ sin_cos.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-time = np.arange(0,10,0.1)
+time = np.arange(0,2*np.pi,0.1)
 sin = np.sin(time)
 cos = np.cos(time)
 plt.plot(time, sin, time, cos)
