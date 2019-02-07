572f9f333676505a3bcc7825ef647c484c6bfd7d
kennethwsherman <47404172+kennethwsherman@users.noreply.github.com>
2019-02-06 20:57:20 -0500

    Ken Sherman HW#2 Update
    update to add tan to the plot and other requirements listed in the assignment

--- sin_cos.py
+++ sin_cos.py
@@ -1,9 +1,14 @@
+#ks indicates the changes that Ken Sherman made to the code
 import numpy as np
 import matplotlib.pyplot as plt
 
 time = np.arange(0,2*np.pi,0.1)
 sin = np.sin(time)
 cos = np.cos(time)
-plt.plot(time, sin, time, cos)
+tan = np.tan(time) #ks added tangent to the graph
+plt.plot(time, sin, time, cos, time, tan)
 plt.axhline(y=0, color = 'black')
+axes = plt.gca()
+axes.set_ylim([-1,1]) #ks setting y axis max and mins so we can see the plots
 plt.show()
+plt.savefig('hw2save.png') #ks this will save the plot locally
